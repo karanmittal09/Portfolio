58140ec31f5c4d5f7233840d0e3f282255c16085
karanmittal09 <151923351+karanmittal09@users.noreply.github.com>
2025-03-27 01:46:09 +0530

    add reveal animation to header and update background color

--- script.js
+++ script.js
@@ -16,6 +16,22 @@ tl.to("#elem", {
 tl.to("#white", {
   height: "100%",
   duration: 2,
-  delay: -1.5,
+  delay: -1.8,
   ease: "expo.inOut"
 });
+
+document.querySelectorAll(".reveal").forEach((elem) => {
+  let spanParent = document.createElement("span");
+  let spanChild = document.createElement("span");
+
+  spanParent.classList.add("parent");
+  spanChild.classList.add("child");
+
+  spanChild.text = elem.textContent;
+  spanParent.appendChild(spanChild);
+
+  elem.innerHTML = "";
+  elem.appendChild(spanParent);
+
+})
+
