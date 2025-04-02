31b021ef21e608bb1fbe20ae67d324cb62e6aae3
karanmittal09 <151923351+karanmittal09@users.noreply.github.com>
2025-04-02 13:57:03 +0530

    add mobile menu

--- script.js
+++ script.js
@@ -48,3 +48,20 @@ function initSwiper() {
     pagination: { el: ".swiper-pagination", clickable: true },
   });
 }
+
+// Mobile Menu
+const navMenu = document.getElementById("nav-menu"),
+navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+  if(navToggle){
+    navToggle.addEventListener("click", ()=> {
+      navMenu.classList.add("shpw-menu");
+    })
+  }
+
+  if (navClose) {
+    navClose.addEventListener("click", () => {
+      navMenu.classList.remove("show-menu");
+    });
+  }
